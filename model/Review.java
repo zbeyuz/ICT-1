239b70bed3a7b94e717c494bdc2908d11377f96e
Wannajak <fou1234@gotmail.com>
2015-08-03 09:26:07 +0800

    add for review model and update all preparestatement
    add add review and get_review
    change register from void to boolean
    minor change

---
+++ model/Review.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author hy
+ */
+public class Review {
+    public int item_id = -1;
+    public int user_id = -1;
+    public String review_title;
+    public Date review_date;
+    public int review_value = -1;
+    public int review_price = -1;
+    public int review_quality = -1;
+    public String review_text="";
+
+}
