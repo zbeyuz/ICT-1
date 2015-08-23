3c727075af5e4ed15d95c9515963723b47d28fb0
Wannajak <Fu@Dariuss-MacBook-Pro.local>
2015-08-23 21:00:21 +0800

    update model for product highlight

---
+++ src/java/test/ProductHighMgrTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import database.ItemMgr;
+
+import java.util.ArrayList;
+import model.ProductBest;
+import model.ProductDeal;
+import model.ProductFeature;
+
+/**
+ *
+ * @author Fu
+ */
+public class ProductHighMgrTest {
+    public static void main(String[] args) throws Exception{
+        ArrayList<ProductFeature> res =  new ArrayList();
+        res = database.ProductHighlight.getProductFeature();
+        System.out.println(res.size());
+    }
+}
