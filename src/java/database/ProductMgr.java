a92cc08529f15c28003b1e9245d314cd75c1a56d
Le Anh Tuan <zbeyuz@gmail.com>
2015-08-23 21:48:19 +0800

    Update model for product highlight

--- src/java/database/ProductMgr.java
+++ src/java/database/ProductMgr.java
@@ -11,7 +11,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import model.Item;
 import model.Product;
 import model.Review;
 
@@ -56,88 +55,6 @@ public class ProductMgr {
             //add fail,already add product_id
         }
         conn.close();
-    }
-
-    public static void addItem(int product_id, String item_material, String item_color, String item_size, String item_sample_pic, int item_quantity) throws SQLException, Exception {
-
-        Connection conn = DBConn.getConn();
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        int item_id = 0;
-
-        pstmt = conn.prepareStatement("SELECT * FROM `product_list` where `product_id` = ?");
-        pstmt.setInt(1, product_id);
-        rs = pstmt.executeQuery();
-        int cg = 0; //use to check that have product_id or not
-        while (rs.next()) {
-            cg = 1;
-        }
-
-        if (cg == 0) {
-
-            pstmt = conn.prepareStatement("SELECT * FROM `item_info` where `product_id` = ?");
-            pstmt.setInt(1, product_id);
-            rs = pstmt.executeQuery();
-
-            while (rs.next()) { //to get item_id
-                item_id = rs.getInt("item_id");
-            }
-            item_id++;
-
-            pstmt = conn.prepareStatement("insert into item_info values(?,?,?,?,?,?,?)");
-
-            pstmt.setInt(1, product_id);
-            pstmt.setInt(2, item_id);
-            pstmt.setString(3, item_material);
-            pstmt.setString(4, item_color);
-            pstmt.setString(5, item_size);
-            pstmt.setString(6, item_sample_pic);
-            pstmt.setInt(7, item_quantity);
-            pstmt.executeUpdate();
-            //add done
-        } else {
-            throw new Exception("Can not add same product id");
-            //add fail,already add product_id
-        }
-        conn.close();
-    }
-
-    public static void updateItem(int product_id, int item_id, String item_material, String item_color, String item_size, String item_sample_pic, int item_quantity) throws SQLException, Exception {
-
-        Connection conn = DBConn.getConn();
-        PreparedStatement pstmt = null;
-
-        int cg = 0;
-
-        if (cg == 0) {
-
-            pstmt = conn.prepareStatement("UPDATE `item_info` SET `item_material`=? ,`item_color`=?,`item_size`=?,`item_sample_pic`=?,`item_quantity`=? WHERE `product_id`=? AND `item_id`=?");
-            pstmt.setString(1, item_material);
-            pstmt.setString(2, item_color);
-            pstmt.setString(3, item_size);
-            pstmt.setString(4, item_sample_pic);
-            pstmt.setInt(5, item_quantity);
-            pstmt.setInt(6, product_id);
-            pstmt.setInt(7, item_id);
-            pstmt.executeUpdate();
-            //add done
-        } else {
-            throw new Exception("Can not add same product id");
-            //add fail,already add product_id
-        }
-        conn.close();
-    }
-
-    public static void deleteItem(int product_id, int item_id) throws SQLException, Exception {
-
-        Connection conn = DBConn.getConn();
-        PreparedStatement pstmt = null;
-
-        pstmt = conn.prepareStatement("DELETE FROM `item_info` where `product_id` = ? and `item_id` = ?");
-        pstmt.setInt(1, product_id);
-        pstmt.setInt(2, item_id);
-        pstmt.executeUpdate();
-
     }
 
     public static void deleteProduct(int product_id) throws SQLException, Exception {
@@ -307,31 +224,6 @@ public class ProductMgr {
         return res;
     }
 
-    public static ArrayList<Item> getItemByProductId(int product_id) throws SQLException, Exception {
-
-        Connection conn = DBConn.getConn();
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        pstmt = conn.prepareStatement("SELECT * FROM `item_info` where product_id = ?");
-        pstmt.setInt(1, product_id);
-        rs = pstmt.executeQuery();
-
-        ArrayList<Item> res = new ArrayList();
-
-        while (rs.next()) {
-            Item i = new Item();
-            i.product_id = rs.getInt("product_id");
-            i.item_id = rs.getInt("item_id");
-            i.item_material = rs.getString("item_material");
-            i.item_color = rs.getString("item_color");
-            i.item_size = rs.getString("item_size");
-            i.item_sample_pic = rs.getString("item_sample_pic");
-            i.item_quantity = rs.getInt("item_quantity");
-            res.add(i);
-        }
-        conn.close();
-        return res;// get arreylist like get product but there are onty 1 product in list
-    }
 
     public static String[] getProductPic(int product_id) throws SQLException, Exception {
 
@@ -354,81 +246,6 @@ public class ProductMgr {
         return pic;
     }
 
-    public static boolean addReview(int product_id, int user_id, String review_title, long review_time, int review_value, int review_price, int review_quality, String review_text) throws SQLException, Exception {
-
-        Connection conn = DBConn.getConn();
-        String ch = "ok";
-        PreparedStatement pstmt = conn.prepareStatement("INSERT INTO `product_review` (`product_id`, `user_id`, `review_title`, `review_date`, `review_value`, `review_price`, `review_quality`, `review_text`) VALUES (?,?,?,FROM_UNIXTIME(?),?,?,?,?)");
-        if (ch.equals("ok")) {
-            pstmt.setInt(1, product_id);
-            pstmt.setInt(2, user_id);
-            pstmt.setString(3, review_title);
-            pstmt.setLong(4, review_time);
-            pstmt.setInt(5, review_value);
-            pstmt.setInt(6, review_price);
-            pstmt.setInt(7, review_quality);
-            pstmt.setString(8, review_text);
-            pstmt.executeUpdate();
-            conn.close();
-            return true;
-        } else {
-            conn.close();
-            return false;
-        }
-    }
-
-    public static ArrayList<Review> getReview(int product_id) throws SQLException, Exception {
-
-        Connection conn = DBConn.getConn();
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        pstmt = conn.prepareStatement("SELECT * FROM `product_review`,`user_list` WHERE `product_review`.`product_id`=? and `product_review`.`user_id`=`user_list`.`user_id`");
-        pstmt.setInt(1, product_id);
-        rs = pstmt.executeQuery();
-
-        ArrayList<Review> res = new ArrayList();
-
-        while (rs.next()) {
-            Review i = new Review();
-            i.product_id = rs.getInt("product_id");
-            i.user.id = rs.getInt("user_id");
-            i.user.email = rs.getString("user_email");
-            i.user.fName = rs.getString("user_fname");
-            i.user.lName = rs.getString("user_lname");
-            i.review_time = rs.getTimestamp("review_date").getTime();
-            i.review_title = rs.getString("review_title");
-            i.review_value = rs.getInt("review_value");
-            i.review_price = rs.getInt("review_price");
-            i.review_quality = rs.getInt("review_quality");
-            i.review_text = rs.getString("review_text");
-            res.add(i);
-        }
-        conn.close();
-        return res;
-    }
-
-    public static Item getItemByItemId(int item_id) throws SQLException, Exception {
-        Connection conn = DBConn.getConn();
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        pstmt = conn.prepareStatement("SELECT * FROM `item_info` where item_id = ?");
-        pstmt.setInt(1, item_id);
-        rs = pstmt.executeQuery();
-
-        Item i = new Item();
-
-        if (rs.next()) {
-            i.product_id = rs.getInt("product_id");
-            i.item_id = rs.getInt("item_id");
-            i.item_material = rs.getString("item_material");
-            i.item_color = rs.getString("item_color");
-            i.item_size = rs.getString("item_size");
-            i.item_sample_pic = rs.getString("item_sample_pic");
-            i.item_quantity = rs.getInt("item_quantity");
-        }
-        conn.close();
-        return i;
-    }
 
     private static ArrayList<Product> productListFromRs(ResultSet rs) throws SQLException {
         ArrayList<Product> res = new ArrayList<Product>();
